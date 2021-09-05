a7f217dd1ea696137a1f197948f41a7bb4f4bf31
MasatoooNkazatooo <Saccersaccer04@icloud.com>
2021-09-05 13:52:38 +0900

    done

---
+++ js-2/app.js
@@ -0,0 +1,13 @@
+let hello = 'Hello World';
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+alert(str1 + float1 + int1 + int2 );
